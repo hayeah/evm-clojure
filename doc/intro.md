68247ce8510da1e5409d4f293bc7e5e0a21bab45
Howard Yeh <hayeah@gmail.com>
2018-06-18 15:15:46 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clll
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clll/core_test.clj
@@ -0,0 +1,11 @@
+(ns clll.core-test
+  (:require [clojure.test :refer :all]
+            [clll.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
+
+(deftest b-test
+  (testing "FIXME, I fail!!"
+    (is (= 1 1))))
